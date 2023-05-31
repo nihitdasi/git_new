3276b55b271fb2d4c64c80033ad918dfe0edacf3
Nihit Dasi <rajender.nihit@bidgera.com>
2023-05-31 13:36:09 +0530

    Commiting dinakar2

---
+++ dinakar2.py
@@ -0,0 +1,10 @@
+a = 3  
+b = 4  
+c = 5  
+  
+# calculate the semi-perimeter  
+s = (a + b + c) / 2  
+  
+# calculate the area  
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5  
+print('The area of the triangle is %0.2f' %area)  
